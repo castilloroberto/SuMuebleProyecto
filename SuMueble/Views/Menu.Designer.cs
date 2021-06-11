36d4802e28bb065e026e064b06e5e2ebba6cf448
Kerencastellanos <kerencastellano468@gmail.com>
2021-06-11 10:45:59 -0600

    Menu casi listo

--- SuMueble/Views/Menu.Designer.cs
+++ SuMueble/Views/Menu.Designer.cs
@@ -29,45 +29,176 @@ namespace SuMueble.Views
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.venta1 = new SuMueble.Views.Venta();
+            this.btn_cerrarSesion = new System.Windows.Forms.Button();
+            this.btn_clientes = new System.Windows.Forms.Button();
+            this.btn_colaboradores = new System.Windows.Forms.Button();
+            this.btn_historialVentas = new System.Windows.Forms.Button();
+            this.btn_devoluciones = new System.Windows.Forms.Button();
+            this.btn_creditos = new System.Windows.Forms.Button();
+            this.btn_inventario = new System.Windows.Forms.Button();
+            this.btn_ventas = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.btn_cerrarSesion);
+            this.panel1.Controls.Add(this.btn_clientes);
+            this.panel1.Controls.Add(this.btn_colaboradores);
+            this.panel1.Controls.Add(this.btn_historialVentas);
+            this.panel1.Controls.Add(this.btn_devoluciones);
+            this.panel1.Controls.Add(this.btn_creditos);
+            this.panel1.Controls.Add(this.btn_inventario);
+            this.panel1.Controls.Add(this.btn_ventas);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1300, 120);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(1924, 202);
+            this.panel1.TabIndex = 2;
             // 
-            // button1
+            // btn_cerrarSesion
             // 
-            this.button1.Location = new System.Drawing.Point(290, 42);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 27);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ventas";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_cerrarSesion.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_cerrarSesion.FlatAppearance.BorderSize = 0;
+            this.btn_cerrarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cerrarSesion.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_cerrarSesion.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrarSesion.Image")));
+            this.btn_cerrarSesion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_cerrarSesion.Location = new System.Drawing.Point(1684, 75);
+            this.btn_cerrarSesion.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_cerrarSesion.Name = "btn_cerrarSesion";
+            this.btn_cerrarSesion.Size = new System.Drawing.Size(218, 56);
+            this.btn_cerrarSesion.TabIndex = 7;
+            this.btn_cerrarSesion.Text = "Cerrar Sesión";
+            this.btn_cerrarSesion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_cerrarSesion.UseVisualStyleBackColor = false;
+            this.btn_cerrarSesion.Click += new System.EventHandler(this.btn_cerrarSesion_Click);
             // 
-            // venta1
+            // btn_clientes
             // 
-            this.venta1.Location = new System.Drawing.Point(53, 135);
-            this.venta1.Name = "venta1";
-            this.venta1.Size = new System.Drawing.Size(1145, 675);
-            this.venta1.TabIndex = 1;
+            this.btn_clientes.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_clientes.FlatAppearance.BorderSize = 0;
+            this.btn_clientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clientes.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_clientes.Location = new System.Drawing.Point(1431, 75);
+            this.btn_clientes.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_clientes.Name = "btn_clientes";
+            this.btn_clientes.Size = new System.Drawing.Size(186, 56);
+            this.btn_clientes.TabIndex = 6;
+            this.btn_clientes.Text = "Clientes";
+            this.btn_clientes.UseVisualStyleBackColor = false;
+            this.btn_clientes.Click += new System.EventHandler(this.btn_clientes_Click);
+            // 
+            // btn_colaboradores
+            // 
+            this.btn_colaboradores.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_colaboradores.FlatAppearance.BorderSize = 0;
+            this.btn_colaboradores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_colaboradores.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_colaboradores.Location = new System.Drawing.Point(1205, 75);
+            this.btn_colaboradores.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_colaboradores.Name = "btn_colaboradores";
+            this.btn_colaboradores.Size = new System.Drawing.Size(222, 56);
+            this.btn_colaboradores.TabIndex = 5;
+            this.btn_colaboradores.Text = "Colaboradores";
+            this.btn_colaboradores.UseVisualStyleBackColor = false;
+            this.btn_colaboradores.Click += new System.EventHandler(this.btn_colaboradores_Click);
+            // 
+            // btn_historialVentas
+            // 
+            this.btn_historialVentas.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_historialVentas.FlatAppearance.BorderSize = 0;
+            this.btn_historialVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_historialVentas.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_historialVentas.Location = new System.Drawing.Point(938, 75);
+            this.btn_historialVentas.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_historialVentas.Name = "btn_historialVentas";
+            this.btn_historialVentas.Size = new System.Drawing.Size(263, 56);
+            this.btn_historialVentas.TabIndex = 4;
+            this.btn_historialVentas.Text = "Historial de Ventas";
+            this.btn_historialVentas.UseVisualStyleBackColor = false;
+            this.btn_historialVentas.Click += new System.EventHandler(this.btn_historialVentas_Click);
+            // 
+            // btn_devoluciones
+            // 
+            this.btn_devoluciones.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_devoluciones.FlatAppearance.BorderSize = 0;
+            this.btn_devoluciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_devoluciones.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_devoluciones.Location = new System.Drawing.Point(720, 75);
+            this.btn_devoluciones.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_devoluciones.Name = "btn_devoluciones";
+            this.btn_devoluciones.Size = new System.Drawing.Size(214, 56);
+            this.btn_devoluciones.TabIndex = 3;
+            this.btn_devoluciones.Text = "Devoluciones";
+            this.btn_devoluciones.UseVisualStyleBackColor = false;
+            this.btn_devoluciones.Click += new System.EventHandler(this.btn_devoluciones_Click);
+            // 
+            // btn_creditos
+            // 
+            this.btn_creditos.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_creditos.FlatAppearance.BorderSize = 0;
+            this.btn_creditos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_creditos.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_creditos.Location = new System.Drawing.Point(530, 75);
+            this.btn_creditos.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_creditos.Name = "btn_creditos";
+            this.btn_creditos.Size = new System.Drawing.Size(186, 56);
+            this.btn_creditos.TabIndex = 2;
+            this.btn_creditos.Text = "Creditos";
+            this.btn_creditos.UseVisualStyleBackColor = false;
+            this.btn_creditos.Click += new System.EventHandler(this.btn_creditos_Click);
+            // 
+            // btn_inventario
+            // 
+            this.btn_inventario.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_inventario.FlatAppearance.BorderSize = 0;
+            this.btn_inventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_inventario.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_inventario.Location = new System.Drawing.Point(340, 74);
+            this.btn_inventario.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_inventario.Name = "btn_inventario";
+            this.btn_inventario.Size = new System.Drawing.Size(186, 56);
+            this.btn_inventario.TabIndex = 1;
+            this.btn_inventario.Text = "Inventario";
+            this.btn_inventario.UseVisualStyleBackColor = false;
+            this.btn_inventario.Click += new System.EventHandler(this.btn_inventario_Click);
+            // 
+            // btn_ventas
+            // 
+            this.btn_ventas.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_ventas.FlatAppearance.BorderSize = 0;
+            this.btn_ventas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ventas.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_ventas.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_ventas.Location = new System.Drawing.Point(150, 74);
+            this.btn_ventas.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_ventas.Name = "btn_ventas";
+            this.btn_ventas.Size = new System.Drawing.Size(186, 56);
+            this.btn_ventas.TabIndex = 0;
+            this.btn_ventas.Text = "Ventas";
+            this.btn_ventas.UseVisualStyleBackColor = false;
+            this.btn_ventas.Click += new System.EventHandler(this.btn_ventas_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 207);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1924, 843);
+            this.panel2.TabIndex = 3;
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 866);
-            this.Controls.Add(this.venta1);
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Menu";
@@ -78,9 +209,15 @@ namespace SuMueble.Views
         }
 
         #endregion
-
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private Venta venta1;
+        private System.Windows.Forms.Button btn_cerrarSesion;
+        private System.Windows.Forms.Button btn_clientes;
+        private System.Windows.Forms.Button btn_colaboradores;
+        private System.Windows.Forms.Button btn_historialVentas;
+        private System.Windows.Forms.Button btn_devoluciones;
+        private System.Windows.Forms.Button btn_creditos;
+        private System.Windows.Forms.Button btn_inventario;
+        private System.Windows.Forms.Button btn_ventas;
+        private System.Windows.Forms.Panel panel2;
     }
 }
