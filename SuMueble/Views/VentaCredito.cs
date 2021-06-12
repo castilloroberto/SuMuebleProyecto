c8613f1350f69ea6adb97b2d4a655a730c9e5135
Dorian_Antonio_Zelaya <dorianvelasques@gmail.com>
2021-06-11 19:23:01 -0600

    terminados las ventanas de ventas al credito y al contado, verificar si estan correctas

---
+++ SuMueble/Views/VentaCredito.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SuMueble.Views
+{
+    public partial class VentaCredito : Form
+    {
+        public VentaCredito()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentaCredito_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
