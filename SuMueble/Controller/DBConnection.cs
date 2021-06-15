817a7577a875c2fc979e1d3295012eb49f7702a1
Kerencastellanos <kerencastellano468@gmail.com>
2021-06-15 14:39:22 -0600

    Agregue Appconfig

--- SuMueble/Controller/DBConnection.cs
+++ SuMueble/Controller/DBConnection.cs
@@ -7,7 +7,7 @@ namespace SuMueble.Views
 {
     public class DBConnection
     {
-        public static string GetcarlosString()
+        public static string GetString()
         {
             return ConfigurationManager.ConnectionStrings[""].ConnectionString;
         }
