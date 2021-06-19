5d047a6ed0fe9f0bae912b947e227acfcbdd5da4
jorge kirkconnell <jorgekirkconnell2001@hotmail.com>
2021-06-19 10:24:28 -0600

    Agregue la clase Puestos

---
+++ SuMueble/Models/Puestos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuMueble.Models
+{
+    public class Puestos
+    {
+        public int ID { get; set; }
+        public string Puesto { get; set; }
+    }
+}
