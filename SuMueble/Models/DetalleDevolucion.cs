d45c6ff6f65f2f764198fa4704b802458237e657
Dorian_Antonio_Zelaya <dorianvelasques@gmail.com>
2021-07-02 22:23:45 -0600

    clase Devolucion

---
+++ SuMueble/Models/DetalleDevolucion.cs
@@ -0,0 +1,33 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuMueble.Models
+{
+    [Table("DetalleDevolucion")]
+    public class DetalleDevolucion
+    {
+
+        public Guid IDDevolucion
+        {
+            set; get;
+        }
+        public int IDProducto
+        {
+            set; get;
+        }
+        public int Cantidad
+        {
+            set; get;
+        }
+        public string Motivo
+        {
+            set; get;
+        }
+        public string Observaciones
+        {
+            set; get;
+        }
+    }
+}
