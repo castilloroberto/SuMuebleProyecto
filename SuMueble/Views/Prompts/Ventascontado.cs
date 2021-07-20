c038ae2cf5266421f07223bdd8a2443259f71827
Kerencastellanos <kerencastellano468@gmail.com>
2021-07-20 09:29:45 -0600

    Detalle de venta al crédito y venta al contado

--- SuMueble/Views/Prompts/Ventascontado.cs
+++ SuMueble/Views/Prompts/Ventascontado.cs
@@ -1,18 +1,31 @@
-﻿using System;
+﻿using SuMueble.Controller;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace SuMueble.Views
 {
     public partial class Ventascontado : Form
     {
+        VentaController ventacontrolador = new VentaController();
+        DetalleVentaController detalleVentaController = new DetalleVentaController();
+        DataTable venta = new DataTable();
+        DataTable DetalleVenta = new DataTable();
+
         public Ventascontado()
         {
+        }
+
+        public Ventascontado(string cod_factura)
+        {
             InitializeComponent();
+            venta = ventacontrolador.GetVenta(cod_factura);
+            //DetalleVenta = detalleVentaController.GetDetalleVenta(venta);
         }
 
         private void label3_Click(object sender, EventArgs e)
