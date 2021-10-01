0e80d8b1467f18000743744f3257f60a66311dab
AndreaPosas <AndreaPosas@ANDREA>
2021-09-30 22:18:13 -0600

    No es nada

--- SuMueble/Views/VentaView.cs
+++ SuMueble/Views/VentaView.cs
@@ -40,7 +40,7 @@ namespace SuMueble.Views
             productos = pc.GetProductos().ToList();
             dgv_productos.DataSource = productos;
         }
-        //Andrea Posas
+        //Andrea Celeste
         private void btn_terminarVenta_Click(object sender, EventArgs e)
         {
             Clientes c = new Clientes()
@@ -235,6 +235,7 @@ namespace SuMueble.Views
             return msg;
 
         }
+        //NO se
 
         private void VentaView_Load(object sender, EventArgs e)
         {
